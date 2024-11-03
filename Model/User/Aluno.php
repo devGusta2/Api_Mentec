0fad6bf4863eec6b407f426d01928767585acea6
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-03 15:49:00 -0300

    Mvc

---
+++ Model/User/Aluno.php
@@ -0,0 +1,10 @@
+<?php
+//class aluno usando os atributos da classe user
+class Aluno extends User{
+
+
+    
+    
+}
+
+?>
---
+++ Model/User/User.php
@@ -0,0 +1,33 @@
+<?php
+class User{
+    //atributos
+    protected string $email;
+    protected string $password;
+    protected string $name;
+
+    
+    // Metodos setters
+    public function setEmail($email){
+        $this ->email = $email;
+    }
+    public function setPassword($password){
+        $this ->password = $password;
+    }
+    public function setName($name){
+        $this-> name= $name;
+    }
+
+    // Metodos getters
+
+    public function getEmail(){
+        return $this->$email;
+    }
+    public function getPassword(){
+        return $this -> $password;
+    }
+    public function getName(){
+        return $this->$name;
+    }
+}
+
+?>
