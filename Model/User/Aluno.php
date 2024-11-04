ad8beea06e185139715c4fcbd4ea32973aeee1db
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-04 13:25:20 -0300

    Signup student

--- Model/User/Aluno.php
+++ Model/User/Aluno.php
@@ -1,10 +1,22 @@
 <?php
+include_once('../Database.php');
 //class aluno usando os atributos da classe user
 class Aluno extends User{
 
+    public function cadastro(){
+        // cria objeto da conexao com o banco
+        $dao = new Database();
+        // pega o metodo da conexao
+        $conn = $dao -> getCon();
 
-    
-    
+        $query = "INSERT INTO tbUser ( 
+        lastNameUser, --ultimo nome
+        roleUser, --papel do usuario no caso fica entre (Mentor, Aluno e Adm)
+        
+        )VALUES($nameUser, $roleUser)";
+
+    }
+
 }
 
 ?>
