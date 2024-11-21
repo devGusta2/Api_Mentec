f93b062d1ac695aeeab735a3e44ff67caa2bd8be
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-21 16:52:26 -0300

    Inserindo alguns dados, porém não todos

--- Controller/Mentorships_Control/Mentorships_controller.php
+++ Controller/Mentorships_Control/Mentorships_controller.php
@@ -3,17 +3,19 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
     header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos permitidos
 
-    include_once('../../Model/Mentorships/Mentorships.php');
-    include_once('../../Model/Dao/Database.php');
+    include('../../Model/Mentorships/Mentorships.php');
+    include('../../Model/Dao/Database.php');
     //função que regista as mmentorias
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $data = json_decode(file_get_contents('php//input'));
+        echo "ta pedindo coisa ali irmao";
+        $data = json_decode(file_get_contents('php://input'));
         //Buscando dados vindo do forms do REACT JS
 
-        // transforma em vetor
+        //transforma em vetor
         $data = [
-            $data-> nome,
+          
             $data-> title,
+            //$data-> nome,
             $data-> description,
             $data-> target,
             $data-> goals,
@@ -30,22 +32,25 @@
             $data-> duration,
             
         ];
-
+      
+        
+        
         //instancia
-        $mentoring = new Mentorships();
+        //$mentoring = new Mentorships();
         $dao = new Database();
         
         //define a conexao;
         $conn = $dao -> getCon();
 
+        $sql = "INSERT INTO mentorias (titulo) VALUES (?,?,?,?,?,?,?,?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$data[0], $data[1], $data[2], $data[3], $data[4], $data[5] ]);
+
         //obtem os dados vindo do JSON
         
-        
 
 
-
-
-        $mentoring -> mentorShipRegistration();
+        //$mentoring -> mentorShipRegistration(foreach($data));
     }
     
 ?>
