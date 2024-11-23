e7fb1063ddead1cde21ecab74ef13cb18ecaa94a
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-22 21:51:14 -0300

    Get request mentorings

--- Controller/Mentorships_Control/Mentorships_controller.php
+++ Controller/Mentorships_Control/Mentorships_controller.php
@@ -5,83 +5,102 @@
 
     include('../../Model/Mentorships/Mentorships.php');
     include('../../Model/Dao/Database.php');
-    //função que regista as mmentorias
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo "ta pedindo coisa ali irmao";
-        $data = json_decode(file_get_contents('php://input'));
-        //Buscando dados vindo do forms do REACT JS
-
-        //transforma em vetor
-        $data = [
-          
-            $data-> title,
-            //$data-> nome,
-            $data-> description,
-
-            $data-> target,
-
-            $data-> goal,
-
-            $data-> content,
-
-            $data-> frequency,
-
-            $data-> requirements,
-
-            $data-> methods,
-
-            $data-> teacher,
-
-            $data-> place,
-
-            $data-> payment,
-
-            $data-> feedback,
-
-            $data-> price,
-
-            $data-> date_begin,
-
-            $data-> duration,
-            
-        ];
-        //instancia
-        //$mentoring = new Mentorships();
-        $dao = new Database();
-        
-        //define a conexao;
-        $conn = $dao -> getCon();
-
-        $sql = "INSERT INTO mentorias (
-            titulo, 
-            descricao,
-            publico_alvo, 
-            objetivos,
-            conteudo_programatico, 
-            frequencia,
-            requisitos,
-            metodologia,
-            instrutor,
-            local,
-            forma_pagamento,
-            feedback,
-            valor,
-            data_inicio,
-            duracao
-        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([
-            $data[0], $data[1], $data[2], $data[3], $data[4], $data[5],
-            $data[6], $data[7], $data[8], $data[9], $data[10], $data[11],
-            $data[12], $data[13], $data[14]
-        ]);
-        
-        //obtem os dados vindo do JSON
-        
 
 
-        //$mentoring -> mentorShipRegistration(foreach($data));
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $mentorships = new Mentorships();
+        $data = $mentorships -> fetchMentorships();
+        echo $data;
+        echo json_encode($data);
     }
+    //Buscando Mentorias
+
+
+//     //função que regista as mmentorias
+//     if($_SERVER['REQUEST_METHOD'] == 'POST'){
+       
+//         $data = json_decode(file_get_contents('php://input'));
+//         //Buscando dados vindo do forms do REACT JS
+
+//         //transforma em vetor
+//         $data = [
+          
+//             $data-> title,
+//             //$data-> nome,
+//             $data-> description,
+
+//             $data-> target,
+
+//             $data-> goal,
+
+//             $data-> content,
+
+//             $data-> frequency,
+
+//             $data-> requirements,
+
+//             $data-> methods,
+
+//             $data-> teacher,
+
+//             $data-> place,
+
+//             $data-> payment,
+
+//             $data-> feedback,
+
+//             $data-> price,
+
+//             $data-> date_begin,
+
+//             $data-> duration,
+            
+//         ];
+
+
+// //função para buscar mentorias na model
+
+
+
+
+
+//         //instancia
+//         //$mentoring = new Mentorships();
+//         $dao = new Database();
+        
+//         //define a conexao;
+//         $conn = $dao -> getCon();
+
+//         $sql = "INSERT INTO mentorias (
+//             titulo, 
+//             descricao,
+//             publico_alvo, 
+//             objetivos,
+//             conteudo_programatico, 
+//             frequencia,
+//             requisitos,
+//             metodologia,
+//             instrutor,
+//             local,
+//             forma_pagamento,
+//             feedback,
+//             valor,
+//             data_inicio,
+//             duracao
+//         ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        
+//         $stmt = $conn->prepare($sql);
+//         $stmt->execute([
+//             $data[0], $data[1], $data[2], $data[3], $data[4], $data[5],
+//             $data[6], $data[7], $data[8], $data[9], $data[10], $data[11],
+//             $data[12], $data[13], $data[14]
+//         ]);
+        
+//         //obtem os dados vindo do JSON
+        
+
+
+//         //$mentoring -> mentorShipRegistration(foreach($data));
+//     }
     
 ?>
