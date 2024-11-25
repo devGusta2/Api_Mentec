7fd085a9a7ba70c0042a4c202408383c2119e70a
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-25 18:04:09 -0300

    Mentorships controller fetching deactivate data

--- Controller/Mentorships_Control/Mentorships_controller.php
+++ Controller/Mentorships_Control/Mentorships_controller.php
@@ -3,11 +3,12 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
     header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos permitidos
 
-    include('../../Model/Mentorships/Mentorships.php');
-    include('../../Model/Dao/Database.php');
+    
+
 
 
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        include_once('../../Model/Mentorships/Mentorships.php');
         $mentorships = new Mentorships();
         $data = $mentorships -> fetchMentorships();
  
@@ -102,5 +103,18 @@
 
 //         //$mentoring -> mentorShipRegistration(foreach($data));
 //     }
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //recebe os dados do REACT
+    $data = json_decode(file_get_contents('php://input'));
+    //define o id da mentoria
+    $id = $data -> id;
+    //busca o diretório no MODEL referente as mentorias
+    include_once('../../Model/Mentorships/Mentorships.php');
+    //cria novo objeto de mentorias
+    $mentorships = new Mentorships();
+    //busca o método de delete
+    $mentorships -> deactivateMentorship($id);
+}
     
 ?>
