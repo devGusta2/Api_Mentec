8ec9552f6ff576b401e6facea4924324cd54cc83
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-27 21:17:19 -0300

    Api_mentec hosted in ininity free, changed database connection config

--- Model/Dao/Database.php
+++ Model/Dao/Database.php
@@ -2,10 +2,10 @@
 
 class Database {
     // Variáveis de conexão com o banco de dados
-    private string $nome = "localhost";
-    private string $password = "";
-    private string $user = "root";
-    private string $database = "mentecDb";
+    private string $nome = "sql100.infinityfree.com";
+    private string $password = "mentec8080";
+    private string $user = "if0_37802286";
+    private string $database = "if0_37802286_mentecdb";
     // Variável da conexão
     private ?PDO $con = null;
 
